51a55a1ab3e5c6528410606ddf64f56b616a9d2a
jackalq <jackalqkimo@yahoo.com.tw>
2026-08-31 10:19:25 +0800

    Restore Flutter Android repository resolution

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -16,15 +16,6 @@ pluginManagement {
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
-
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "9.1.0" apply false
